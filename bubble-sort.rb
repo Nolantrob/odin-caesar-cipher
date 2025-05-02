44ff0b8f8d2423b89cc842ac5fb4267429b05827
Nolantrob <nolanrobertson97@gmail.com>
2025-05-01 22:29:20 -0400

    Complete bubble sort project

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -5,21 +5,29 @@ def bubble_sort(array)
 
     limit = array.size
 
-    # This code will loop through the array and perform a swap if current_elem is greater than next_elem
-        for element in 0..limit
+    for iteration in 1..array.size-1
+        swap_happened = false
 
+        # This code will loop through the array and perform a swap if current_elem is greater than next_elem
+        for element in 0..limit
             current_element = array[element]
             next_element = array[element+1]
 
             if (current_element && next_element)
                 if (current_element > next_element)
                     array[element], array[element+1] = array[element+1],array[element]
+                    swap_happened = true
                 end
+            # If there is no next element, we're at the end of the array, so we move onto the next iteration
             else 
                 next
             end
-            puts "Iteration #{element + 1}: #{array}\n Swap Count: #{swap_counter}\n\n"    
         end
+        puts "Iteration #{iteration}: #{array}\n\n"
+        if swap_happened == false
+            break
+        end    
+    end
 
 
 
