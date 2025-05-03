054e30bccc519e421b6f9329a4b7a1db09c4096d
Nolantrob <nolanrobertson97@gmail.com>
2025-05-03 02:28:13 -0400

    Add break if an iteration did not include a swap

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -3,16 +3,17 @@ bubble_array = [4,3,78,2,0,2]
 def bubble_sort(array)
 
 
-    # Create a loop that will run as many times as there are elements in the array - 1
     (array.length - 1).times do |i|
+        swapped_happened = false
 
         (0...array.length - 1 - i).each do |element|
-
             if array[element] > array[element+1]
                 array[element], array[element+1] = array[element+1], array[element]
+                swapped_happened = true
             end
 
         end
+        break unless swapped_happened
     end
     array
 end
