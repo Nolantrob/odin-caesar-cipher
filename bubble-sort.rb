79487fa6c805d73ef32222f4ab35000f29ef2574
Nolantrob <nolanrobertson97@gmail.com>
2025-05-03 02:23:39 -0400

    Overhaul and simplify bubble sort

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -3,34 +3,18 @@ bubble_array = [4,3,78,2,0,2]
 def bubble_sort(array)
 
 
-    limit = array.size
+    # Create a loop that will run as many times as there are elements in the array - 1
+    (array.length - 1).times do |i|
 
-    for iteration in 1..array.size-1
-        swap_happened = false
+        (0...array.length - 1 - i).each do |element|
 
-        # This code will loop through the array and perform a swap if current_elem is greater than next_elem
-        for element in 0..limit
-            current_element = array[element]
-            next_element = array[element+1]
-
-            if (current_element && next_element)
-                if (current_element > next_element)
-                    array[element], array[element+1] = array[element+1],array[element]
-                    swap_happened = true
-                end
-            # If there is no next element, we're at the end of the array, so we move onto the next iteration
-            else 
-                next
+            if array[element] > array[element+1]
+                array[element], array[element+1] = array[element+1], array[element]
             end
+
         end
-        puts "Iteration #{iteration}: #{array}\n\n"
-        if swap_happened == false
-            break
-        end    
     end
-
-
-
+    array
 end
 
-bubble_sort(bubble_array)
+p bubble_sort(bubble_array)
